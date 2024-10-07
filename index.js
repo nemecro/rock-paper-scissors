172f97db0c2d38c00d372dc404884e5407702175
nemecro <nemecstudies@gmail.com>
2024-10-07 10:55:45 +0200

    feat: create getRandomIntInclusive function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+function getComputerChoice() {
+    // randomly return "rock", "paper" or "scissors"
+    // generate a random number ranging from 1 to 3
+    // if the generated random number is 1, return "rock"
+    // if the generated random number is 2, return "paper"
+    // if the generated random number is 3, return "scissors"
+}
+
+function getRandomIntInclusive(min, max) {
+    // take two integers to set the range boundary and generate a random number inclusive the boundary points
+    return Math.round(Math.random() * (max - min) + min);
+    // return Math.floor(Math.random() * (max - min + 1) + min); // + 1 causes the max number to be inclusive, since floor returns LESS THAN or EQUAL TO
+}
+
+// TESTS
+console.log(getRandomIntInclusive(1, 3));
